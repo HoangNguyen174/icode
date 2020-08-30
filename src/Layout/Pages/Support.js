63f9e74c8b057faefe1b7ce333a22584389dcee5
Blackhuy0701 <61944758+Blackhuy0701@users.noreply.github.com>
2020-08-30 16:40:01 +0700

    form2

--- src/Layout/Pages/Support.js
+++ src/Layout/Pages/Support.js
@@ -1,103 +1,89 @@
 
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import './Support.css';
+import TextField from "@material-ui/core/TextField";
+import Checkbox from '@material-ui/core/Checkbox';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import "./Support.css";
 
 export default function Support() {
   return (
     <React.Fragment>
       <Container maxWidth="md" component="main">
         Support
-        <div className="container">
-  <header className="header">
-    <h1 id="title" className="text-center">iCode Support Form</h1>
-    <p id="description" class="description text-center">
-      Thank you for taking the time
-    </p>
-  </header>
-  <form id="survey-form">
-    <div className="form-group">
-      <label id="name-label" for="name">Name</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        class="form-control"
-        placeholder="Enter your name"
-        required
-      />
-    </div>
-    <div className="form-group">
-      <label id="email-label" for="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        class="form-control"
-        placeholder="Enter your Email"
-        required
-      />
-    </div>
-    <div className="form-group">
-      <label id="number-label" for="number"
-        >Age<span class="clue">(optional)</span></label>
-      <input
-        type="number"
-        name="age"
-        id="number"
-        min="10"
-        max="99"
-        class="form-control"
-        placeholder="Age"
-      />
-    </div>
-    <div className="form-group">
-      <p>Which option best describes your current role?</p>
-      <select id="dropdown" name="role" class="form-control" required>
-        <option disabled selected value>Select current role</option>
-        <option value="student">Student</option>
-        <option value="job">Full Time Job</option>
-        <option value="learner">Full Time Learner</option>
-        <option value="preferNo">Prefer not to say</option>
-        <option value="other">Other</option>
-      </select>
-    </div>
-
-    <div className="form-group">
-      <p>Would you like to recieve our news through email?</p>
-      <label>
-        <input
-          name="user-recommend"
-          value="definitely"
-          type="radio"
-          class="input-radio"
-          checked
-        />Yes sure</label>
-      <label>
-        <input
-          name="user-recommend"
-          value="not-sure"
-          type="radio"
-          class="input-radio"
-        />No thanks</label>
-    </div>
-    <div className="form-group">
-      <p>What is your issue?</p>
-      <textarea
-        id="comments"
-        class="input-textarea"
-        name="comment"
-        placeholder="Enter your message here..."
-      ></textarea>
-    </div>
-
-    <div className="form-group">
-      <button type="submit" id="submit" class="submit-button">
-        Submit
-      </button>
-    </div>
-  </form>
-</div>
+        <Container 
+        maxWidth="sm" 
+        style={{ backgroundColor: '#51d1e1', height: '75vh' }}>
+            <h1 className="title">iCode Support Form</h1>
+            <h2 className="title">Thank you for taking time!</h2>
+          <TextField 
+            label="Name"
+            fullWidth 
+            variant="outlined"
+            placeholder="Enter your name"
+            style={{ margin: 8 }}
+            required id="standard-required"
+          />
+          <TextField 
+            label="Age"
+            variant="outlined"
+            placeholder="Your age"
+            style={{margin: 8}}
+          />
+          <TextField 
+            label="Email"
+            variant="outlined"
+            placeholder="Your email"
+            style={{ margin: 8 }}
+            required id="standard-required"
+          />
+          <TextField 
+            label="Job"
+            variant="outlined"
+            placeholder="Your job"
+            style={{ margin: 8 }}
+          />
+          <TextField 
+            label="Phone number"
+            variant="outlined"
+            placeholder="Your phone number"
+            style={{ margin: 8 }}
+          />
+          <TextField 
+            label="Message"
+            variant="outlined"
+            placeholder="Your message"
+            style={{ margin: 8 }}
+            fullWidth
+            multiline="true"
+          />
+          <FormLabel component="legend" style={{ margin: 8}} className="title">
+            Would you like to receive our news via email?
+          </FormLabel>
+          <FormControlLabel
+            value="Yes"
+            control={<Checkbox color="primary" />}
+            label="Yes"
+            labelPlacement="Yes"
+            margin={{ margin: 8 }}
+          />
+          <FormControlLabel
+            value="No"
+            control={<Checkbox color="secondary" />}
+            label="No"
+            labelPlacement="No"
+            margin={{ margin: 8 }}
+          />
+           <Button 
+            variant="contained" color="primary"
+            fullWidth>Submit
+          </Button>
+        </Container>
       </Container>
     </React.Fragment>
   )
